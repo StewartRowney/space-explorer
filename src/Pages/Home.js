df982bb1f8275d09666327d087525af6666ef5e0
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2024-01-19 09:58:51 +0000

    Initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import "./Pages.css"
+
+const Home = () => {
+    return (
+        <div className="home-background">
+
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Pages/NoPage.js
@@ -0,0 +1,10 @@
+
+const NoPage = () => {
+    return (
+        <div>
+            <h1>You have explored too far!</h1>
+        </div>
+    )
+}
+
+export default NoPage;
